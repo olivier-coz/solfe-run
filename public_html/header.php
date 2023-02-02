0e304d95652cba1ba2138609a9ab449a17bc50f1
Olivier Coz <63406293+olivier-coz@users.noreply.github.com>
2023-02-02 15:22:47 +0100

    Update header.php

--- public_html/header.php
+++ public_html/header.php
@@ -7,12 +7,12 @@
     <div class="logo">
       <!-- <h1 class="text-light"><a href="index.php"><span>GOODBYWORLD!</span></a></h1> -->
       <!-- Uncomment below if you prefer to use an image logo -->
-      <a href="run.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+      <a href="index.php"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
     </div>
 
     <nav id="navbar" class="navbar">
       <ul>
-        <li><a class="nav-link scrollto <?php echo $currentPage == "run" ? "active" : "" ?>" href="./run.php">Solfé Run</a></li>
+        <li><a class="nav-link scrollto <?php echo $currentPage == "run" ? "active" : "" ?>" href="./index.php">Solfé Run</a></li>
         <li><a class="nav-link scrollto <?php echo $currentPage == "quiz" ? "active" : "" ?>" href="./quiz.php">Pantoufle</a></li>
         
       </ul>
